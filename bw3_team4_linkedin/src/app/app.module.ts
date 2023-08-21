d90baeb168c000ac6015340fe19338fb0fc784f1
sigurnick <nicolas94.m@gmail.com>
2023-08-21 15:09:21 +0200

    1.0

--- bw3_team4_linkedin/src/app/app.module.ts
+++ bw3_team4_linkedin/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfiloComponent } from './components/profilo/profilo.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfiloComponent
+    ProfiloComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
