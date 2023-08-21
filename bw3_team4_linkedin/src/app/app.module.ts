4d5b26bfce2eea27c62ebd4744bb4d41ab49c29b
ValerioParatore <ValerioTeoParatore@gmail.com>
2023-08-21 17:33:36 +0200

    navbar giorno 1

--- bw3_team4_linkedin/src/app/app.module.ts
+++ bw3_team4_linkedin/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfiloComponent } from './components/profilo/profilo.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfiloComponent
+    ProfiloComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
