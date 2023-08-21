166a4b10108b69ddeb27fb6e294a32950251ce45
Mirko Parente <133377396+mirkoparente@users.noreply.github.com>
2023-08-21 13:51:35 +0200

    card bottom

--- bw3_team4_linkedin/src/app/app.module.ts
+++ bw3_team4_linkedin/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfiloComponent } from './components/profilo/profilo.component';
+import { BottomCardComponent } from './components/bottom-card/bottom-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfiloComponent
+    ProfiloComponent,
+    BottomCardComponent
   ],
   imports: [
     BrowserModule,
