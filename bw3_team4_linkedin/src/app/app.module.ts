e0ffd8ac5df7018ca2747d5c8464c7b5e6786c94
Mirko Parente <133377396+mirkoparente@users.noreply.github.com>
2023-08-21 10:23:10 +0200

    component

--- bw3_team4_linkedin/src/app/app.module.ts
+++ bw3_team4_linkedin/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfiloComponent } from './components/profilo/profilo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfiloComponent
   ],
   imports: [
     BrowserModule,
