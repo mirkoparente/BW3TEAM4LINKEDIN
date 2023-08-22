4b8d3cb36f072d5179c1758b165c27fd044ae976
sigurnick <nicolas94.m@gmail.com>
2023-08-22 11:02:04 +0200

    1.0

--- bw3_team4_linkedin/src/app/app-routing.module.ts
+++ bw3_team4_linkedin/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfiloComponent } from './components/profilo/profilo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'profilo', component: ProfiloComponent
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
